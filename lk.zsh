c5ba38bfd2d3865e37c326a777d7536c64523746
pavelsefcik <pavelsefcik@gmail.com>
2026-03-26 13:26:29 +0100

    Add files via upload

--- lk.zsh
+++ lk.zsh
@@ -1,5 +1,13 @@
 lk() {
-  python3 ~/.lk/lk_helper.py "$@" </dev/tty >/dev/tty
+  case "$1" in
+    -h|--help)
+      nano ~/.lk/README.md
+      ;;
+    -d|--data)
+      nano ~/.lk/lk_data.json
+      ;;
+    *)
+      python3 ~/.lk/lk_helper.py "$@" </dev/tty >/dev/tty
+      ;;
+  esac
 }
-
-alias lk_data="nano ~/.lk/lk_data.json"
