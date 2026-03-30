9ba712dd56e7746b920262d7d658a6e69f5ed42f
pavelsefcik <pavelsefcik@gmail.com>
2026-03-30 23:10:48 +0200

    requirements.txt

--- lk/lk.zsh
+++ lk/lk.zsh
@@ -1,9 +1,2 @@
-# lk v0.21 - terminal bookmarking with TUI
-
-# noglob prevents zsh from expanding glob characters (e.g. ? in URLs)
-# before passing arguments to the function
 _LK_DIR="${0:A:h}"
-_lk() {
-  python3 "$_LK_DIR/lk_helper.py" "$@" </dev/tty >/dev/tty
-}
-alias lk='noglob _lk'
+alias lk="noglob '$_LK_DIR/lk_helper.py'"
